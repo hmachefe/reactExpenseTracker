ba8d53ef66343abb18d9b4b3676380e31345166c
Hugo Machefer <Hugo.Machefer@difuze.com>
2023-01-30 15:36:12 -0500

    add boiler plate features, to forge hello world empty shell

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2>
+        Let's get started
+      </h2>
+    </div>
+  );
+}
+
+export default App;
