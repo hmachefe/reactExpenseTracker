39ad5c1fd4fccc9aa18020601af8b53b1a6c0d79
Hugo Machefer <Hugo.Machefer@difuze.com>
2023-02-01 17:56:27 -0500

    replace hard-coded constants in JS by dynamic expenses array. Propagated via props from App to child ExpenseItem comp

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,15 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-    const expensePrice = "294.68";
-    const expenseDescription = "Car Insurance";
-    const expenseDate = new Date(2021, 2, 28);
-    return (
-        <div className="expense-item">
-        <div>{expenseDate.toISOString()}</div>
-        <div className="expense-item__description">
-            <h2>{expenseDescription}</h2>
-            <div className="expense-item__price">{expensePrice}$</div>
-        </div>
-        </div>
-    );
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}$</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
