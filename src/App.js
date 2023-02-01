f29a29019a3d7d115bc785535519604bb8961bc2
Hugo Machefer <Hugo.Machefer@difuze.com>
2023-01-31 20:47:20 -0500

    add expense item component as primary building block

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
-    <div>
-      <h2>
-        Let's get started
-      </h2>
-    </div>
+    <ExpenseItem />
   );
 }
 
