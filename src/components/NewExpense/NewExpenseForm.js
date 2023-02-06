4e8511dcd90ebd3ce3c3b61cf75f6cb7afce30c4
Hugo Machefer <Hugo.Machefer@difuze.com>
2023-02-06 13:20:08 -0500

    create states for amount and date also triggered on change handling from form

--- src/components/NewExpense/NewExpenseForm.js
+++ src/components/NewExpense/NewExpenseForm.js
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import './NewExpenseForm.css';
 
 function NewExpenseForm() {
 
+    const [enteredTitle, setEnteredTitle] = useState(''); 
     const onTitleChangeHandler = (event) => {
-        console.log('onTitleChangeHandler   event.target.value == ', event.target.value)
+        setEnteredTitle(event.target.value);
+    }
+
+    const [enteredAmount, setEnteredAmount] = useState(''); 
+    const onAmountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+    }
+
+    const [enteredDate, setEnteredDate] = useState(''); 
+    const onDateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
     }
 
     return (
@@ -15,11 +27,11 @@ function NewExpenseForm() {
                     </div>
                     <div className="new-expense__control">
                         <label>Amount</label>
-                        <input type="number" min="0.01" step="0.01" />
+                        <input type="number" min="0.01" step="0.01" onChange={onAmountChangeHandler} />
                     </div>                    
                     <div className="new-expense__control">
                         <label>Date</label>
-                        <input type="date" min="2019-01-01" max="2023-12-01" />
+                        <input type="date" min="2019-01-01" max="2023-12-01" onChange={onDateChangeHandler} />
                     </div>                                        
                 </div>                
                 <div className="new-expense__actions">
