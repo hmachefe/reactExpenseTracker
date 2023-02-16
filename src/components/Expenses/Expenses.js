b2cb10e861dc14045b794c8c833e769c726f4c6a
Hugo Machefer <Hugo.Machefer@difuze.com>
2023-02-15 21:20:06 -0500

    UDEMY-63/UDEMY-64/UDEMY-65/rendering-lists-of-data-using-stateful-lists-undestanding-keys

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,35 @@
+import React from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+
+    const [filteredDate, setFilteredDate] = React.useState('2021');
+
+    const onDateFilter = (selectedDate) => {
+        console.log('in Expenses.js     selectedDate == ', selectedDate);
+        setFilteredDate(selectedDate);
+    }
+
     return (
-        <Card className="expenses">
-            {
-                props.expenses.map((expense, index) => {
-                    return(<ExpenseItem key={index} title={expense.title} amount={expense.amount} date={expense.date} />);
-                })
-            }
-        </Card>
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter selectedDate={filteredDate} onDateChange={onDateFilter}/>
+                {
+                    props.expenses.filter(expense => {
+                        console.log('expense.date.getFullYear() == ', expense.date.getFullYear().toString());
+                        console.log('filteredDate == ', filteredDate.toString());
+                        return expense.date.getFullYear().toString() === filteredDate.toString()
+                    }).map((expense, index) => {
+                        return(<ExpenseItem key={index} title={expense.title} amount={expense.amount} date={expense.date} />);
+                    })
+                }
+            </Card>
+        </div>
+
+
     )
 }
 export default Expenses;
